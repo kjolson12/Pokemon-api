0ab8151473db845a1e08dfe6fac5e4caa7de0cda
kjolson12 <kobaseball02@gmail.com>
2020-08-02 15:33:22 -0700

    Pokemon api finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getAllPokemon, getPokemon } from './services/pokemon';
 import Card from './components/Card/';
 import './App.css';
+import Navbar from './components/Navbar/';
 
 function App() {
   const [pokemonData, setPokemonData] = useState([]);
@@ -22,6 +23,25 @@ function App() {
     fetchData();
   }, []);
 
+  const next = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const prev = async () => {
+    if(!prevUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
   const loadingPokemon = async (data) => {
     let _pokemonData = await Promise.all(data.map(async pokemon => {
       let pokemonRecord = await getPokemon(pokemon.url);
@@ -34,10 +54,19 @@ function App() {
     <div>
       {loading ? <h1>Loading...</h1> : (
         <>
+          <Navbar />
+          <div className="btn">
+            <button onClick={prev}>Prev</button>
+            <button onClick={next}>Next</button>
+          </div>
           <div className="grid-container">
             {pokemonData.map((pokemon, i) => {
               return <Card key={i} pokemon={pokemon} />
             })}
+          </div>
+          <div className="btn">
+            <button onClick={prev}>Prev</button>
+            <button onClick={next}>Next</button>
           </div>
         </>
       )}
